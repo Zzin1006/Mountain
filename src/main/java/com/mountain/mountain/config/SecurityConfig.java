5c531742dab22f6915358ffec7f9d6a6c733ad3d
jiinahn92@gmail.com <jiinahn92@gmail.com>
2022-01-02 22:43:53 +0900

    feat : HttpSecurity

--- src/main/java/com/mountain/mountain/config/SecurityConfig.java
+++ src/main/java/com/mountain/mountain/config/SecurityConfig.java
@@ -5,10 +5,14 @@ import com.mountain.mountain.domain.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity // 스프링 시큐리티 필터가 스프링 필터체인에 등록이 됨
@@ -21,10 +25,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private AuthFilterContainer authFilterContainer;
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable() // CSRF 보호기능 disable
+                .authorizeRequests() // 요청에 대한 권한 지정
+                .anyRequest().authenticated() // 모든 요청이 인증되어야한다.
+                .and()
+                .addFilterBefore(authFilterContainer.getFilter(),
+                        UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling() //예외처리 기능 작동
+                .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)); // 인증실패시처리
+    };
+
+    @Override
     public void configure(WebSecurity web) throws Exception {
         //인증 예외 URL 설정
 
         web.ignoring().antMatchers(HttpMethod.POST,"users")
                 .antMatchers("/");
     }
+
 }
