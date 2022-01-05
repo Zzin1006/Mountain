1d258b55f5715d6cfbfe6df607fcb9a690ddd93f
jiinahn92@gmail.com <jiinahn92@gmail.com>
2022-01-05 17:44:58 +0900

    feat : add login

--- src/main/java/com/mountain/mountain/controller/user/UserController.java
+++ src/main/java/com/mountain/mountain/controller/user/UserController.java
@@ -2,12 +2,14 @@ package com.mountain.mountain.controller.user;
 
 
 import com.mountain.mountain.controller.user.dto.RegisterUserDTO;
+import com.mountain.mountain.controller.user.dto.UserDTO;
 import com.mountain.mountain.domain.user.model.User;
 import com.mountain.mountain.domain.user.service.UserService;
 import com.mountain.mountain.util.RequestUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -54,6 +56,16 @@ public class UserController {
                     "{\"code\":\"INVALID_TOKEN\", \"message\":\"" + e.getMessage() + "\"}");
         }
 
+
     }
+
+
+    // 로그인
+    @GetMapping("/me")
+    public UserDTO login(Authentication authentication) {
+        User loginUser = (User) authentication.getPrincipal();
+        return new UserDTO(loginUser);
+    }
+
 
 }
