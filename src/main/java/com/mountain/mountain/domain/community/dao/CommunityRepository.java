2055f449eadfb0ab065b426c97196812488ed2ab
jiinahn92@gmail.com <jiinahn92@gmail.com>
2022-01-11 15:01:31 +0900

    feat: delete Community

--- src/main/java/com/mountain/mountain/domain/community/dao/CommunityRepository.java
+++ src/main/java/com/mountain/mountain/domain/community/dao/CommunityRepository.java
@@ -1,12 +1,12 @@
 package com.mountain.mountain.domain.community.dao;
 
 import com.mountain.mountain.domain.community.model.Community;
+import com.mountain.mountain.domain.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CommunityRepository extends JpaRepository<Community, Long> {
-
 
 
 }
--- src/main/java/com/mountain/mountain/domain/community/service/CommunityService.java
+++ src/main/java/com/mountain/mountain/domain/community/service/CommunityService.java
@@ -5,6 +5,8 @@ import com.mountain.mountain.domain.category.model.Category;
 import com.mountain.mountain.domain.community.dao.CommunityRepository;
 import com.mountain.mountain.domain.community.model.Community;
 import com.mountain.mountain.domain.user.model.User;
+import com.mountain.mountain.exception.CustomException;
+import com.mountain.mountain.exception.ErrorCode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -26,4 +28,14 @@ public class CommunityService {
     }
 
 
+    public void deleteCommunity(User user, Long commupostNo) {
+        Community commu = communityRepository.findById(commupostNo)
+                .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_COMMUNITY));
+
+        if(commu.getWriterId().getId().equals(user.getId())) {
+            communityRepository.delete(commu);
+        } else {
+            throw new CustomException(ErrorCode.FORBIDDEN_USER);
+        }
+    }
 }
